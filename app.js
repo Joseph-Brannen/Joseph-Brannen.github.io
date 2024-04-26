17c67d83b08156cbd9bae20bebd89e3102462bc5
Joseph-Brannen <141746228+Joseph-Brannen@users.noreply.github.com>
2024-04-26 11:09:45 -0400

    Add all modals

--- app.js
+++ app.js
@@ -8,3 +8,39 @@ projectOne.addEventListener('click', (e) => {
     modalContainerOne.style.display = 'grid';
   }
 });
+
+
+const modalContainerTwo = document.getElementById('modalcontainertwo');
+const modalTwo = document.getElementById('modaltwo');
+const projectTwo = document.getElementById('projecttwo');
+
+projectTwo.addEventListener('click', (e) => {
+  const button = document.getElementById('btntwo');
+  if ( button === e.target ) {
+    modalContainerTwo.style.display = 'grid';
+  }
+});
+
+
+const modalContainerThree = document.getElementById('modalcontainerthree');
+const modalThree = document.getElementById('modalthree');
+const projectThree = document.getElementById('projectthree');
+
+projectThree.addEventListener('click', (e) => {
+  const button = document.getElementById('btnthree');
+  if ( button === e.target ) {
+    modalContainerThree.style.display = 'grid';
+  }
+});
+
+
+const modalContainerFour = document.getElementById('modalcontainerfour');
+const modalFour = document.getElementById('modalfour');
+const projectFour = document.getElementById('projectfour');
+
+projectFour.addEventListener('click', (e) => {
+  const button = document.getElementById('btnfour');
+  if ( button === e.target ) {
+    modalContainerFour.style.display = 'grid';
+  }
+});
